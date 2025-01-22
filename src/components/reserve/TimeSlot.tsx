a03abd0516518fadc7ab7240b14e77d16bd9a581
Gabriele Leonardi <leonardi.gabriele.lg@gmail.com>
2025-01-22 10:47:40 +0100

    unavailable slots if past

--- src/components/reserve/TimeSlot.tsx
+++ src/components/reserve/TimeSlot.tsx
@@ -23,11 +23,17 @@ const TimeSlot = ({ guests = 0, date = "", time, timeZone }: Props) => {
     useOdoo,
   );
 
+  const [hours, minutes] = time.from.split(":").map(Number);
+  const now = new Date();
+  const past =
+    now.getHours() > hours ||
+    (now.getHours() === hours && now.getMinutes() >= minutes);
+
   return (
     <Radio
       value={JSON.stringify(time)}
       className="custom-underline cursor-pointer data-[disabled]:pointer-events-none data-[selected]:bg-grey data-[disabled]:text-darkgrey"
-      isDisabled={!availableTables?.length}
+      isDisabled={!availableTables?.length || past}
     >
       <div className="grid grid-flow-col items-end justify-between p-xs">
         <Text tag="div">{time.from}</Text>
